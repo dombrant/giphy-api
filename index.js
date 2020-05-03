e1c51b6f32164c97ac7038117b43716bc102ce66
Dominic Brant <dombrant@gmail.com>
2020-05-03 15:43:09 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import giphyApi from "giphy-js-sdk-core";
+
+const app = express();
+dotenv.config();
+const client = giphyApi(`${process.env.GIPHY_API_KEY}`);
+
+const gifRequest = async (query) => {
+  const request = await client.search("gifs", { q: query });
+  const randomElement =
+    request.data[Math.floor(Math.random() * request.data.length)];
+
+  return randomElement.images.downsized_large.url;
+};
+
+app.get("/", async (request, response) => {
+  try {
+    const gif = await gifRequest("chris+farley");
+    response.send(`<img src = ${gif}></img>`);
+  } catch (error) {
+    response.send(`Error: ${error}`);
+  }
+});
+
+app.get("/:search", async (request, response) => {
+  const gif = await gifRequest(request.params.search);
+  response.send(`<img src = ${gif}></img>`);
+});
+
+app.listen(3000, () => console.log("Server listening on port 3000"));
