732eecaec253b938225232ebb77ef0c772f8b7d4
Dominic Brant <dombrant@gmail.com>
2020-05-08 13:28:50 -0700

    temporarily made a hellow world response

--- functions/giphy-api.js
+++ functions/giphy-api.js
@@ -1,9 +1,6 @@
-const express = require("express");
 const dotenv = require("dotenv");
 const giphyApi = require("giphy-js-sdk-core");
-const serverless = require("serverless-http");
 
-const app = express();
 dotenv.config();
 const client = giphyApi(`${process.env.GIPHY_API_KEY}`);
 
@@ -15,21 +12,23 @@ const gifRequest = async (query) => {
   return randomElement.images.downsized_large.url;
 };
 
-app.get("/", async (request, response) => {
-  try {
-    const gif = await gifRequest("chris+farley");
-    response.send(`<img src = ${gif}></img>`);
-  } catch (error) {
-    response.send(`Error: ${error}`);
-  }
-});
+// app.get("/", async (request, response) => {
+//   try {
+//     const gif = await gifRequest("chris+farley");
+//     response.send(`<img src = ${gif}></img>`);
+//   } catch (error) {
+//     response.send(`Error: ${error}`);
+//   }
+// });
 
-app.get("/:search", async (request, response) => {
-  const gif = await gifRequest(request.params.search);
-  response.send(`<img src = ${gif}></img>`);
-});
+// app.get("/:search", async (request, response) => {
+//   const gif = await gifRequest(request.params.search);
+//   response.send(`<img src = ${gif}></img>`);
+// });
 
-app.listen(3000, () => console.log("Server listening on port 3000"));
-
-exports.handler = serverless(app);
-// For the Netlify function
+exports.handler = function (event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: "Hello, World",
+  });
+};
