9844edd3ba263d71e680b229954956d36b4005bf
Dominic Brant <dombrant@gmail.com>
2021-06-29 15:50:20 -0700

    reset after accidental pull from master

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-import express from "express";
-import dotenv from "dotenv";
-import giphyApi from "giphy-js-sdk-core";
+const dotenv = require("dotenv");
+const giphyApi = require("giphy-js-sdk-core");
 
-const app = express();
 dotenv.config();
 const client = giphyApi(`${process.env.GIPHY_API_KEY}`);
 
@@ -14,18 +12,16 @@ const gifRequest = async (query) => {
   return randomElement.images.downsized_large.url;
 };
 
-app.get("/", async (request, response) => {
+exports.handler = async (event, context) => {
   try {
-    const gif = await gifRequest("chris+farley");
-    response.send(`<img src = ${gif}></img>`);
+    const query = event.queryStringParameters.search || "chris+farley";
+    const gif = await gifRequest(query);
+
+    return {
+      statusCode: 200,
+      body: gif,
+    };
   } catch (error) {
-    response.send(`Error: ${error}`);
+    return { statusCode: 500, body: error };
   }
-});
-
-app.get("/:search", async (request, response) => {
-  const gif = await gifRequest(request.params.search);
-  response.send(`<img src = ${gif}></img>`);
-});
-
-app.listen(3000, () => console.log("Server listening on port 3000"));
+};
